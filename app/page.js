ba1fe26537ad9617a3a7024fa532903a7392a672
al-ameen36 <muhdabdullahi36@gmail.com>
2023-06-08 18:40:31 +0100

    FIX: fix font styling & sizing

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import About from '@/components/about'
 import Banner from '@/components/banner'
+import Education from '@/components/education'
 
 export default function Home() {
   return (
     <main>
       <Banner />
       <About />
+      <Education />
     </main>
   )
 }
