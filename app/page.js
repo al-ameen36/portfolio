eaace57d08d6427dac34edb9d2d6041de18aec1a
al-ameen36 <muhdabdullahi36@gmail.com>
2023-06-08 18:23:46 +0100

    ADD: add aboout section and styling

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import About from '@/components/about'
 import Banner from '@/components/banner'
 
 export default function Home() {
   return (
     <main>
       <Banner />
+      <About />
     </main>
   )
 }
