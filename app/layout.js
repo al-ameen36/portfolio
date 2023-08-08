10b4937858af1f39cded615a9e95fc44257f230e
al-ameen <muhdabdullahi36@gmail.com>
2023-08-08 12:07:41 +0100

    Fix: bebas neue font not displaying

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,37 @@
-import Link from 'next/link'
-import './globals.scss'
-import navStyles from '@/styles/nav.module.scss'
-import { Bebas_Neue, Montserrat } from 'next/font/google'
+import Link from "next/link";
+import "./globals.scss";
+import navStyles from "@/styles/nav.module.scss";
+import { Montserrat, Bebas_Neue } from "next/font/google";
 
-const bebas = Bebas_Neue({ subsets: ['latin'], weight: '400' })
-const montserrat = Montserrat({ subsets: ['latin'] })
+const bebas = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+  variable: "--font-bebas-neue",
+});
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Muhammad Al-Ameen\'s portfolio',
-  description: 'About Muhammad Al-Ameen Abdullahi professionally',
-}
+  title: "Muhammad Al-Ameen's portfolio",
+  description: "About Muhammad Al-Ameen Abdullahi professionally",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>
+      <body className={montserrat.className + " " + bebas.variable}>
         <nav className={navStyles.nav}>
-          <div><Link className={navStyles.nav_brand} href='/'>M.A.A</Link></div>
-          <div><input type='checkbox' name='darkmode' /></div>
+          <div>
+            <Link className={navStyles.nav_brand} href="/">
+              M.A.A
+            </Link>
+          </div>
+          <div>
+            <input type="checkbox" name="darkmode" />
+          </div>
         </nav>
         {children}
       </body>
     </html>
-  )
+  );
 }
